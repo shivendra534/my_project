a768852f54c1081c14155792bd68ce9b56b9606e
shivendra534 <shivendratiwari534@gmail.com>
2025-12-06 23:14:52 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,arr[10];
+    printf("enter lenght of array:\n");
+    scanf("%d\n",&a);        
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("%d",*(arr[0]));
+    
+    return 0;
+}
